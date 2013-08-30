355b9d9284849ee7a936fec26ea65d7a30f2cd96
Adam <adampalay@gmail.com>
2013-08-29 22:18:41 -0400

    MVP?

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, url_for, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
@@ -11,11 +12,13 @@ db = SQLAlchemy(app)
 def home():
 	return "Hello"
 
-@app.route('/<int:slide_id>')
-def show_slide(slide_id=None):
-	if not slide_id:
-		return url_for("home")
-	return redirect(url_for("home"))
+@app.route('/<page_slug>')
+def show_slide(page_slug):
+	page = Page.query.filter(Page.slug == page_slug).first()
+	if page is None:
+		return redirect(reverse("home"))
+	return render_template("test.html", page=page)
+
 
 if __name__ == '__main__':
 	app.run()
