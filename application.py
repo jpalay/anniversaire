6062aee880b29772f3737ecd4246454ba82d4d55
Adam <adampalay@gmail.com>
2013-08-29 22:40:26 -0400

    change structure and add __init__.py

--- application.py
+++ application.py
@@ -1,18 +1,13 @@
-import os
-from flask import Flask, url_for, render_template, request, redirect
-from flask.ext.sqlalchemy import SQLAlchemy
+from momdad import app
+from flask import redirect, reverse, render_template
 from models import Page
 
-app = Flask(__name__)
-print 1
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
-print 2
-db = SQLAlchemy(app)
 
 print "prehome"
 @app.route('/')
 def home():
 	return "Hello"
+
 print "pre slug"
 @app.route('/<page_slug>')
 def show_page(page_slug):
@@ -24,5 +19,3 @@ def show_page(page_slug):
 	return render_template("test.html", page=page)
 	print "post render"
 
-if __name__ == '__main__':
-	app.run()
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-import application.db as db
+from momdad import db
 
 class Media(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
